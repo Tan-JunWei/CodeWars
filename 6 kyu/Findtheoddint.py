eb288675e1eba5ae232b28bb5f519ba7be6b2082
jwtan <jwtan0402@gmail.com>
2024-07-08 07:49:07 +0800

    algorithms practice

--- 6 kyu/Findtheoddint.py
+++ 6 kyu/Findtheoddint.py
@@ -21,7 +21,7 @@ def find_it(seq):
         if i %2 != 0:
             index = new_list.index(i)
     return list(unique_char)[index]
-find_it([20,1,1,2,2,3,3,5,5,4,20,4,5])
+print(find_it([20,1,1,2,2,3,3,5,5,4,20,4,5]))
 find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])
 
 '''
---
+++ practice/matrix_multiplication.py
@@ -0,0 +1,25 @@
+A = [[1,2,3],
+     [4,5,6],
+     [7,8,9]]
+
+B = [[2,0,0],
+     [0,2,0],
+     [0,0,2]]
+
+result = []
+
+def matrixmult(A,B):
+    for i in range(len(A)):
+        numlist = []
+        for k in range(len(B[0])):
+            sum = 0
+            for j in range(len(B)):
+                sum += A[i][k] * B[k][j]
+            numlist.append(sum)
+        result.append(numlist)
+    print(result)
+
+matrixmult(A,B)
+
+# expected:
+# [[2, 4, 6], [8, 10, 12], [14, 16, 18]]
